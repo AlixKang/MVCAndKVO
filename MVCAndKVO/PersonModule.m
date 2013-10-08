375d435d0d1bf1a4e673f7da43dd1ec2ae247c10
Guanglu.Kang <alix.kanglu@gmail.com>
2013-10-08 11:30:31 +0800

    1. 数据有效性检测

--- MVCAndKVO/PersonModule.m
+++ MVCAndKVO/PersonModule.m
@@ -16,24 +16,69 @@
 - (int)getYear{
     NSDateFormatter* formatter = [NSDateFormatter new];
     formatter.dateFormat = @"yyyy";
-    return [formatter stringFromDate:self].integerValue;
+    return [formatter stringFromDate:self].intValue;
 }
 
 @end
 
 @implementation PersonModule
 - (void)setTelephone:(NSString *)telephone{
+    if ([_telephone isEqualToString:telephone]) {
+        return;
+    }
     // 判断有效性
+    NSString* expression = @"[1]{1}[358]{1}[0-9]{9}";
+    NSError *error = NULL;
     
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:expression
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&error];
     
+    if (error) {
+        return;
+    }
+    NSTextCheckingResult *match = [regex firstMatchInString:telephone
+                                                    options:0
+                                                      range:NSMakeRange(0, telephone.length)];
+    
+    if (match){
+        _telephone = telephone;
+    }
 }
 - (void)setEmail:(NSString *)email{
+    if ([_email isEqualToString:email]) {
+        return;
+    }
     // 判断有效性
-}
-- (void)setAge:(int)age{
+    NSString *expression = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,5}";
+    NSError *error = NULL;
     
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:expression
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&error];
+    
+    if (error) {
+        return;
+    }
+    NSTextCheckingResult *match = [regex firstMatchInString:email
+                                                    options:0
+                                                      range:NSMakeRange(0, email.length)];
+    
+    if (match){
+        _email = email;
+    }
 }
-
+- (void)setBirthday:(NSDate *)birthday{
+    if ([_birthday isEqual:birthday]) {
+        return;
+    }
+    int year = birthday.getYear;
+    int yearNow = [NSDate date].getYear;
+    yearNow -= year;
+    if (year > 0 && year < 125) {
+        _birthday = birthday;
+    }
+}
 - (int)age{
     NSDate* now = [NSDate date];
     return [now getYear] - _birthday.getYear;
